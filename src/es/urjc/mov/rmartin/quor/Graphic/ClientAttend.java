9b9733efccaf9a501fbb73ff8b3637a60192ca1e
ralvarezmar <r.alvarezmar@gmail.com>
2018-05-14 21:48:28 +0200

    threads por cliente

--- src/es/urjc/mov/rmartin/quor/Graphic/ClientAttend.java
+++ src/es/urjc/mov/rmartin/quor/Graphic/ClientAttend.java
@@ -10,11 +10,31 @@ public class ClientAttend implements Runnable {
 	private DataInputStream in;
 	private DataOutputStream out;
 	private String nick;
+	private SocketAddress id;
 	
-	public ClientAttend(Socket s) {
-		
+	public ClientAttend(Socket s,String nick) {
+		this.incon = incon;
+		this.nick = nick;
+		try {
+			this.idata = new DataInputStream(incon.getInputStream());
+			this.odata = new DataOutputStream(incon.getOutputStream());
+			//this.id = incon.getRemoteSocketAddress().toString().split(":")[1];	
+			this.id = incon.getRemoteSocketAddress();
+		} catch (IOException e) {
+			closeAll();
+			thread.interrupt();
+			throw new RuntimeException(this + "open streams: " + e);
+		}		
+	}
+	public String getId() {
+		return id;
 	}
 	
+	private void ReceiveAndSend() {
+		for(;;) {
+			
+		}
+	}
 	
 	@Override
 	public void run() {
--- src/es/urjc/mov/rmartin/quor/Graphic/Server.java
+++ src/es/urjc/mov/rmartin/quor/Graphic/Server.java
@@ -23,11 +23,14 @@ public class Server{
 	private static final int PORT = 2020;
 	private ServerSocket socket;
 	private Thread thread;
-	private int numGames=0;
-	Map<String, SocketAddress> clientsMap = new HashMap<String, SocketAddress>();
-	private Client clients[] = new Client[2];
-	private ArrayList<Game> partidas = new ArrayList<Game>();	
-	int turno=0;
+	public int numGames=0;
+	public static Map<String, SocketAddress> clientsMap = new HashMap<String, SocketAddress>();
+	public static Map<SocketAddress, Thread> socketThreads = new HashMap<SocketAddress, Thread>();
+
+	public static Client clients[] = new Client[2];
+	public ArrayList<Game> partidas = new ArrayList<Game>();	
+	public static int turno=0;
+	
 	
 	public Server(){
 		try {
@@ -37,102 +40,17 @@ public class Server{
 		}
 	}	
 
-	private Boolean isClientFree(String nick) {
-		if(clientsMap.get(nick)==null) {
-			return true;
-		}
-		return false;
-	}
-	
-	private void loginMessage(Message message,Socket s) throws IOException {
-		Login login = (Login) message;
-		String nick=login.getNick();
-		if(isClientFree(nick)) {
-			System.out.println("Client accepted: " + nick);
-			SocketAddress ip = s.getRemoteSocketAddress();
-			Client client = new Client(ip,nick);
-			clientsMap.put(nick, ip);
-			OkLogin ok = new OkLogin(turno);
-			OutputStream writer= s.getOutputStream();
-		    DataOutputStream out=new DataOutputStream(writer);
-			ok.writeTo(out);
-			out.close();
-			if(clients[0]==null) { 
-				clients[turno]=client;
-				turno++;
-			}else if(clients[1]==null){
-				clients[turno]=client;
-				Game game = new Game(numGames,clients[0],clients[1]);
-				partidas.add(game);
-				numGames++;
-				turno=0;
-				clients = new Client[2];
-				System.out.println("Empieza partida");
-			}			
-		}else {
-			System.out.println("Client rejected: " + nick);
-			ErrorMessage error = new ErrorMessage();
-			OutputStream writer= s.getOutputStream();
-		    DataOutputStream out=new DataOutputStream(writer);
-			error.writeTo(out);
-			out.close();
-		}
-	}
-	
-	private void sendMove(Message message,Client client) {
-	    SocketAddress sockaddr = client.ip;
-	    try {
-			Socket s = new Socket();
-			s.connect(sockaddr);
-			OutputStream writer= s.getOutputStream();
-		    DataOutputStream out=new DataOutputStream(writer);
-		    message.writeTo(out);
-		    out.close();
-		    s.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private void playMessage(Message message) {
-		Play play = (Play) message;
-		String nick=play.getNick();
-		if(!isClientFree(nick)) {
-			if(clients[0].nick==nick) {
-				sendMove(message,clients[1]);
-			}else {
-				sendMove(message,clients[0]);
-			}
-		}
-	}
-	
 	private void connect() {
 		try{
-			System.out.println("wait clients ...");
-			
+			System.out.println("wait clients ...");			
 			for(;;){
 				Socket s = socket.accept();
-			    InputStream reader= s.getInputStream();
-		        DataInputStream o=new DataInputStream(reader);
-				Message message = Message.ReadFrom(o);	
-				System.out.println("Mensaje recibido: " + message);
-				switch(message.type()){
-					case LOGIN:
-						loginMessage(message,s);
-						break;
-					case PLAY:
-						playMessage(message);
-						break;
-					case ERROR:
-						break;
-					default:
-						break;
-				}
-				o.close();
+				thread = new Thread(new ClientAt(s));
+				thread.start();				
 			}
 		} catch (IOException e) {
 			System.err.println("connection error: " + e);
-		}/* finally{
+		} finally{
 			try{
 				if(socket != null){
 					socket.close();
@@ -140,7 +58,7 @@ public class Server{
 			} catch (IOException e) {
 				System.err.println("error to close: " + e);  	
 			}
-		}*/
+		}
 	}
 
 	private void start(){
@@ -151,8 +69,115 @@ public class Server{
 		};
 		thread.start();
 	}
+
+	public static void main(String[] args) {
+		new Thread(){
+			public void run(){
+				Server s = new Server();
+				s.start();
+			}		
+		}.start();		
+	}		
 	
-/*
+	
+	private class ClientAt implements Runnable {
+
+		private Socket socket;
+		private DataInputStream in;
+		private DataOutputStream out;
+		private String nick;
+		private SocketAddress id;
+		
+		public ClientAt(Socket socket) {
+			this.socket=socket;
+			try {
+				this.in = new DataInputStream(socket.getInputStream());
+				this.out = new DataOutputStream(socket.getOutputStream());
+				//this.id = incon.getRemoteSocketAddress().toString().split(":")[1];	
+				this.id = socket.getRemoteSocketAddress();
+			} catch (IOException e) {
+				throw new RuntimeException(this + "open streams: " + e);
+			}		
+		}
+
+		private Boolean isClientFree(String nick) {
+			if(clientsMap.get(nick)==null) {
+				return true;
+			}
+			return false;
+		}
+		
+		private void loginMessage(Message message) throws IOException {
+			Login login = (Login) message;
+			String nick=login.getNick();
+			if(isClientFree(nick)) {
+				System.out.println("Client accepted: " + nick);
+				Client client = new Client(id,nick);
+				clientsMap.put(nick, id);
+				OkLogin ok = new OkLogin(turno);
+				ok.writeTo(out);
+				if(clients[0]==null) { 
+					clients[turno]=client;
+					turno++;
+				}else if(clients[1]==null){
+					clients[turno]=client;
+					Game game = new Game(numGames,clients[0],clients[1]);
+					partidas.add(game);
+					numGames++;
+					turno=0;
+					clients = new Client[2];
+					System.out.println("Empieza partida");
+				}			
+			}else {
+				System.out.println("Client rejected: " + nick);
+				ErrorMessage error = new ErrorMessage();
+				error.writeTo(out);
+			}
+		}
+		
+		private void sendMove(Message message,Client client) {
+		    SocketAddress sockaddr = client.ip;
+		    try {
+		    	//recuperar thread del otro cliente y mandarlo a través de ese 
+				Socket s = new Socket();
+				s.connect(sockaddr);
+			    message.writeTo(out);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		private void playMessage(Message message) {
+			Play play = (Play) message;
+			String nick=play.getNick();
+			if(!isClientFree(nick)) {
+				if(clients[0].nick==nick) {
+					sendMove(message,clients[1]);
+				}else {
+					sendMove(message,clients[0]);
+				}
+			}
+		}
+		private void receiveMessages() throws IOException {
+			for(;;) {
+				Message message = Message.ReadFrom(in);	
+				System.out.println("Mensaje recibido: " + message);
+				switch(message.type()){
+					case LOGIN:
+						loginMessage(message);
+						break;
+					case PLAY:
+						playMessage(message);
+						break;
+					case ERROR:
+						System.out.println("Error!");
+						break;
+					default:
+						break;
+				}
+			}
+		}
+		
 		public void closeAll(){
 			try {
 				if(in != null){
@@ -162,29 +187,56 @@ public class Server{
 				if(out != null){
 					out.close();
 					out = null;
+				}	
+				if(socket != null){
+					socket.close();
+					socket = null;
 				}
-				if(incon != null){
-					incon.close();
-					incon = null;
-				}
-				if(outcon!=null) {
-					outcon.close();
-					outcon=null;
-				}
-				} catch (IOException e) {
+			} catch (IOException e) {
 				System.err.println(this + ": " + e);  
 			}
 		}
+		
+		@Override
+		public void run() {
+			try{
+				receiveMessages();	
+			} catch (Exception e) {
+				System.out.println(e.getMessage());;	
+			} finally{
+				closeAll();
+			}			
+		}		
+
+		public SocketAddress getId() {
+			return id;
+		}		
+		public Socket getS() {
+			return socket;
+		}
+		public void setS(Socket s) {
+			this.socket = s;
+		}
+		public DataInputStream getIn() {
+			return in;
+		}
+		public void setIn(DataInputStream in) {
+			this.in = in;
+		}
+		public DataOutputStream getOut() {
+			return out;
+		}
+		public void setOut(DataOutputStream out) {
+			this.out = out;
+		}
+		public String getNick() {
+			return nick;
+		}
+		public void setNick(String nick) {
+			this.nick = nick;
+		}
 	}
-*/
-	public static void main(String[] args) {
-		new Thread(){
-			public void run(){
-				Server s = new Server();
-				s.start();
-			}		
-		}.start();		
-	}		
+
 }
 
 
